ea9b6bd51c17dbb16217c89a5e928982f74b3acb
GrepoData <admin@grepodata.com>
2020-02-12 19:29:04 +0100

    Frontend CI

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 timestamp=$(date +%y%m%d_%H%M%S)
+projectdir="/home/vps/grepodata/acceptance/grepodata-backend"
+dirname="dist_v${timestamp}"
 
 # Make dir
-cd "/home/vps/grepodata/acceptance/grepodata-backend"
-dirname="dist_v${timestamp}"
+cd "$projectdir"
 echo "=== Creating new directory: ${dirname}"
 mkdir "$dirname" || exit 1
 
@@ -11,12 +12,9 @@ mkdir "$dirname" || exit 1
 echo "=== Cloning grepodata-backend to directory: ${dirname}"
 cd "$dirname"
 git init .
-git remote add -t \* -f origin https://github.com/grepodata/grepodata-backend/ || exit "$?"
+git remote add -t \* -f origin https://github.com/GrepoDataTools/grepodata-backend/ || exit "$?"
 git checkout develop || exit "$?"
 git log -1
-
-echo "=== Moving config"
-cp config.private.php "${dirname}/Software/config.private.php" || exit 1
 
 # Composer install
 echo "=== Running composer install"
@@ -24,8 +22,12 @@ composer install
 
 # Update active
 if [ -f vendor/autoload.php ]; then
+  cd "$projectdir"
+
+  echo "=== Moving config to ${dirname}/Software/config.private.php"
+  cp config.private.php "${dirname}/Software/config.private.php" || exit 1
+
   echo "=== Updating active syslink to: ${dirname}"
-  cd "/home/vps/grepodata/acceptance/grepodata-backend"
   rm active
   ln -s "$dirname" active
 else
