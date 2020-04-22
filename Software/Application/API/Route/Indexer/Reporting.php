59b07f93c090a3ce2f4e034ffbe2c1d40d790c38
GrepoData <admin@grepodata.com>
2020-04-22 20:10:07 +0200

    Bug report endpoint

---
+++ Software/Application/API/Route/Indexer/Reporting.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Grepodata\Application\API\Route\Indexer;
+
+use Grepodata\Library\Logger\Logger;
+
+class Reporting extends \Grepodata\Library\Router\BaseRoute
+{
+  public static function BugReportPOST()
+  {
+    try {
+      $aParams = self::validateParams(array('error', 'function', 'browser', 'version', 'world', 'index'));
+
+      // Save bug report as logmessage
+      Logger::warning("INDEXER_BUG_REPORT => " . json_encode($aParams));
+
+      die(self::OutputJson(array('success' => true), 200));
+    } catch (\Exception $e) {
+      Logger::warning("Unable to save bug report: " . $e->getMessage());
+      die(self::OutputJson(array('success' => true), 200));
+    }
+  }
+
+}
