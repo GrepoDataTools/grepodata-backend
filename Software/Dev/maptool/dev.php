5e891722ddbd80428106f896caeaf82a175932ab
CamielK <camielk@live.nl>
2022-09-24 23:54:12 +0200

    Boilerplate code to build map data

--- Software/Dev/maptool/dev.php
+++ Software/Dev/maptool/dev.php
@@ -11,3 +11,20 @@ if (PHP_SAPI !== 'cli') {
 require(__DIR__ . '/../config.php');
 
 Logger::enableDebug();
+
+//$world_id = "nl97";
+//$oWorld = World::getWorldById($world_id);
+
+
+$aWorlds = array(
+  World::getWorldById("nl97")
+);
+
+/** @var \Grepodata\Library\Model\World $oWorld */
+foreach ($aWorlds as $oWorld) {
+  // Check commands 'php SCRIPTNAME[=0] WORLD[=1]'
+  if (isset($argv[1]) && $argv[1]!=null && $argv[1]!='' && $argv[1]!=$oWorld->grep_id) continue;
+
+  //todo: generate map data
+
+}
