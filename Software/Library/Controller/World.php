dd3cdd925d0e4a28b27cab48301c813d6e883bdd
CamielK <camielk@live.nl>
2024-01-14 12:10:59 +0100

    Delete Danish server (no more active worlds)

--- Software/Library/Controller/World.php
+++ Software/Library/Controller/World.php
@@ -8,8 +8,7 @@ use Grepodata\Library\Logger\Logger;
 
 class World
 {
-  private static $aServers = array('ar', 'br', 'cz', 'de', 'dk', 'en', 'es', 'fr', 'gr', 'hu', 'it',
-    'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'se', 'sk', 'tr', 'us', 'zz');
+  private static $aServers = array('ar', 'br', 'cz', 'de', 'en', 'es', 'fr', 'gr', 'hu', 'it', 'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'se', 'sk', 'tr', 'us', 'zz');
 
   /**
    * @param $WorldId
